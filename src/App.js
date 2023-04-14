db122a95a275c369509460401913cb0cef436359
GRACE0211 <110452979+GRACE0211@users.noreply.github.com>
2023-04-14 22:28:51 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -57,11 +57,11 @@ export default class App extends React.Component {
           <Route path='login' element={<Login/>}/>
         </Routes>
 
-        <div class=" h-10 bg-amber-400 fixed bottom-0 w-full">
-          <div class="text-center pt-2">
-              <span class="ml-2">&copy; GRACE JHUO</span>
-              <span class="ml-2">聯絡信箱</span>
-              <span class="ml-2">關於我</span>
+        <div className=" h-10 bg-amber-400 fixed bottom-0 w-full">
+          <div className="text-center pt-2">
+              <span className="ml-2">&copy; GRACE JHUO</span>
+              <span className="ml-2">聯絡信箱</span>
+              <span className="ml-2">關於我</span>
           </div>
         </div>
         </ShopContextProvider>
